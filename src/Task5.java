6ecea760c67bd752ed833b96b9b55f076078ce77
mechmedved <unclebear@yandex.ru>
2024-10-25 20:01:25 +0300

    поправил пятое задани

--- src/Task5.java
+++ src/Task5.java
@@ -1,40 +1,40 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 public class Task5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> input = new ArrayList<String>();
+        ArrayList<String> userInput = new ArrayList<>();
 
         for (int i = 0; i < 5; i++) {
-            input.add(scanner.nextLine());
+            userInput.add(scanner.nextLine());
         }
+        scanner.close();
 
-        System.out.println(checkLengths(input));
+        checkOrder(userInput);
     }
 
-    public static String checkLengths(ArrayList<String> input){
-        AtomicInteger previousLength = new AtomicInteger(-1);
-        AtomicInteger index = new AtomicInteger(0);
-
-        ArrayList<String> result = (ArrayList<String>) input.stream()
-                .filter(w -> {
-                    int currentLength = w.length();
-                    boolean isGreater = previousLength.get() < currentLength;
-                    previousLength.set(currentLength);
-                    if (!isGreater) {
-                        index.set(input.indexOf(w));
-                    }
-                    return isGreater;
-                })
-                .collect(Collectors.toList());
-
-        if (result.size() == input.size()) {
-            return "Список упорядочен";
-        } else {
-            return "Индекс элемента (нарушающего упорядоченность): " + index.get();
+    public static void checkOrder(ArrayList<String> userInput){
+        if (isListOrdered(userInput)) {
+            System.out.println("List ordered");
         }
+    }
+
+    public static boolean isListOrdered(ArrayList<String> userInput){
+
+        int previousLength = -1;
+
+        for(int i=0; i<userInput.size(); i++) {
+            int currentLength = userInput.get(i).length();
+            boolean isGreater = previousLength < currentLength;
+            previousLength = currentLength;
+            if(!isGreater) {
+                System.out.printf("List not ordered : %d", i);
+                return false;
+            }
+        }
+
+        return true;
     }
 }
